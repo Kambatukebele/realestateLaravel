e449afa83a2dff7db03dbcc8ecf150fae7cbeaf6
KAMBA  TUKEBELE <91629041+Kambatukebele@users.noreply.github.com>
2023-06-24 19:54:43 +0200

    update

---
+++ resources/views/components/hero.blade.php
@@ -0,0 +1,61 @@
+<div class="hero">
+  <div class="hero__images">
+    <div class="hero__image">
+      <img class="hero__image-img"
+        src="https://images.squarespace-cdn.com/content/v1/556332e3e4b0e65363e82953/1473115587679-WCXMY9NERI8L9KMBJGVL/1-Martis-Camp-Home-91-Back-2.jpg?format=2500w"
+        alt="">
+      <div class="hero__info">
+        <span class="hero__subtitle">Residential</span>
+        <span class="hero__title">234 Grand Ave</span>
+        <a href="#" class="hero__info-link">See Property</a>
+      </div>
+    </div>
+
+    <div class="hero__image">
+      <img class="hero__image-img"
+        src="https://cdn.onekindesign.com/wp-content/uploads/2019/02/Mountain-Modern-Modular-Home-Hills-Grant-00-1-Kindesign.jpg"
+        alt="">
+      <div class="hero__info">
+        <span class="hero__subtitle">Residential</span>
+        <span class="hero__title">234 Grand Ave</span>
+        <a href="#" class="hero__info-link">See Property</a>
+      </div>
+    </div>
+
+    <div class="hero__image">
+      <img class="hero__image-img" src="https://i.pinimg.com/originals/55/da/50/55da5058c849243292219f4da55322c3.jpg"
+        alt="">
+      <div class="hero__info">
+        <span class="hero__subtitle">Residential</span>
+        <span class="hero__title">Residential</span>
+        <a href="#" class="hero__info-link">Residential</a>
+      </div>
+    </div>
+  </div>
+  <div class="hero__indicator">
+    <div class="hero__indicator-item">
+      <span class="hero__indicator-number">
+        01
+      </span>
+      <span class="hero__indicator-text">
+        Summer Villa
+      </span>
+    </div>
+    <div class="hero__indicator-item">
+      <span class="hero__indicator-number">
+        02
+      </span>
+      <span class="hero__indicator-text">
+        Summer Villa
+      </span>
+    </div>
+    <div class="hero__indicator-item">
+      <span class="hero__indicator-number">
+        03
+      </span>
+      <span class="hero__indicator-text">
+        Summer Villa
+      </span>
+    </div>
+  </div>
+</div>
